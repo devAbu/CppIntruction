1ef22d5af71628570ddabcd300534169c937c2f6
Abdurahman Almonajed <abdulrahman.almonajed@gmail.com>
2019-03-02 19:01:58 +0100

    nisam mogo ranije

---
+++ predavanje1.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{/*
+    cout<<"Hello"<<endl;
+    
+    cout<<"Hello";
+    
+    int a,b = 3;
+    
+    a = 5;
+    
+    cout<<endl<< a + b;
+    
+    int rezultat;
+    
+    rezultat = a + b;
+    
+    cout<<endl<<rezultat;
+    
+    int predmet1 = 5, predmet2 = 3, predmet3 = 4, predmet4 = 5, predmet5 = 5, zbirOcjena, prosjek;
+    
+    zbirOcjena = predmet1 + predmet2 + predmet3 + predmet4 + predmet5;
+    
+    prosjek =  (predmet1 + predmet2 + predmet3 + predmet4 + predmet5) / 5;
+    
+    cout<<endl<<prosjek;
+    
+    int struja,voda,ukupanTrosak;
+    struja = 50;
+    voda = 50;
+    ukupanTrosak= struja+voda;//100
+    cout<<endl<<"mjesecni trosak je "<<ukupanTrosak;
+    ukupanTrosak=ukupanTrosak/30;//3
+    cout<<endl<<"dnevni trosak je "<<ukupanTrosak;
+    */
+    /*
+    int x,y;
+    
+    cout<<"unesi x ";
+    cin>>x;
+    cout<<"unesi y ";
+    cin>>y;
+    
+    cout<<x + y;
+    
+    int struja,voda,ukupanTrosak;
+    cout<<"unesi trosak struje ";
+    cin>>struja;
+    cout<<"unesi trosak vode ";
+    cin>>voda;
+   ukupanTrosak=struja+voda;
+   cout<<ukupanTrosak; 
+    ukupanTrosak= ukupanTrosak/30;
+    cout<<endl<<ukupanTrosak;
+    */
+    
+    int a = 5,b = 5;
+    
+    if(a < b)
+     cout<<"a je manji";
+    if(b<a)
+     cout<<"b je manji";
+    
+   return 0;  
+}
